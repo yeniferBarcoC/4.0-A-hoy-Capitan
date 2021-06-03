0541a55d682972fd1531ae8223fda9fdd2e94cc4
yeniferbarco <>
2021-06-03 02:51:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+""" Programa para apoyar al marinero Seijo
+    Oscar Franco-Bedoya
+    Mayo 3-2021 """
+
+import utilidades as util
+
+def probar_funciones():
+  criatura= util.aparecer_criatura()
+  direccion=util.aparecer_direccion()
+  print(criatura, direccion)
+
+#======================================================================
+#   Algoritmo principal Punto de entrada a la aplicación (Conquistar)
+# =====================================================================
+
+# Ejecuta el programa varias veces para ver su funcionamiento
+
+probar_funciones()
+
+
+
+
+
---
+++ utilidades.py
@@ -0,0 +1,31 @@
+""" Modulo generador aleatorio de cadenas #
+    Funciones que retornan criaturas marinas
+    y ubicaciones del barco 
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+
+import random
+# Definición de Funcione
+def aparecer_criatura():
+  """ 
+  Returns
+  -------
+  criatura:str
+  Una de las 8 criaturas de la lista generadas aleatoriamente     
+  """
+  criaturas=["Kraken","Sirenas","Ballena","Hipocampo","Macaraprono","Pulpo","Leviatanes","Hidras"]
+  indice = random.randint(0, 7)
+  return criaturas[indice]
+
+def aparecer_direccion():
+  """ 
+  Returns
+  -------
+  criatura:str
+  Una de las 4 direcciones de la lista generadas aleatoriamente     
+  """
+  direccion=["babor","estribor","proa","popa"]
+  indice = random.randint(0, 3)
+  return direccion[indice]
+
+
